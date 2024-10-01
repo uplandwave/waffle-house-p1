05c8682bb83cf7d53df69aa45e5df96efec2fd87
Grant Matai Cross <grantmatai@gmail.com>
2024-10-01 14:20:16 -0600

    GMC - first card

--- src/js/product.js
+++ src/js/product.js
@@ -2,8 +2,15 @@ import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+  let cart = localStorage.getItem("so-cart");
+  try{
+    cart = cart ? JSON.parse(cart) : [];
+  } catch (e) {
+    cart = [];
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
+  }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
