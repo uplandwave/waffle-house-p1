e9992981f3dc64170fee114b0da9d2b26d435cab
starb520 <97459015+starb520@users.noreply.github.com>
2024-10-16 19:59:04 -0600

    created a second modal to invite the user to register for the website to enter a giveaway

--- src/js/alerts.js
+++ src/js/alerts.js
@@ -24,6 +24,7 @@ function createAlertModal(alerts) {
   closeButton.innerHTML = "&times;";
   closeButton.onclick = () => {
     document.body.removeChild(modalOverlay);
+    createRegistrationModal();
   };
 
   // lets you close the pop up
@@ -44,6 +45,38 @@ function createAlertModal(alerts) {
   // append modal to overlay and add to body
   modalOverlay.appendChild(modalContainer);
   document.body.appendChild(modalOverlay);
+}
+
+function createRegistrationModal() {
+  const modalOverlay = document.createElement("div");
+  modalOverlay.classList.add("registration-modal-overlay");
+
+  const modalContainer = document.createElement("div");
+  modalContainer.classList.add("registration-modal");
+
+  const closeButton = document.createElement("span");
+  closeButton.classList.add("close-button");
+  closeButton.innerHTML = "&times;";
+  closeButton.onclick = () => {
+    document.body.removeChild(modalOverlay);
+  };
+
+  const message = document.createElement("p");
+  message.textContent = "Register for our site to enter an exclusive giveaway!";
+  message.classList.add("registration-alert-message");
+
+  modalContainer.appendChild(closeButton);
+  modalContainer.appendChild(message);
+  modalOverlay.appendChild(modalContainer);
+  document.body.appendChild(modalOverlay);
+
+  // Add event listener to the overlay to close modal on click
+  modalOverlay.onclick = function (event) {
+    // If the user clicks directly on the overlay (not the modal content), close it
+    if (event.target === modalOverlay) {
+      document.body.removeChild(modalOverlay);
+    }
+  };
 }
 
 function initAlerts() {
