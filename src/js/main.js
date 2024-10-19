4e1a613c3a34e36eb38160ec54aada8260d081d7
mbower00 <mitchbbower@gmail.com>
2024-10-19 17:27:25 -0600

    Newsletter Sign Up Box

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // main.js
 import ProductList from "./components/ProductList.svelte";
+import NewsletterSignUp from "./components/NewsletterSignUp.svelte"
 import { updateCartIcon } from "./utils.mjs";
 
 /**
@@ -9,6 +10,9 @@ new ProductList({
   target: document.querySelector(".products"),
   props: { category: "tents" },
 });
+new NewsletterSignUp({
+  target: document.getElementById("newsletter-sign-up")
+})
 
 // for the cart to update properly we need to check the contence of the cart
 // on the home page. If we dont, the display tag in CSS will stay on none
