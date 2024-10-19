42bf44d2fa8926f6153333270477f5fce97982bc
mbower00 <mitchbbower@gmail.com>
2024-10-19 17:27:25 -0600

    Newsletter Sign Up Box

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 // main.js
 import ProductList from "./components/ProductList.svelte";
-import { updateCartIcon, renderHeaderFooter } from "./utils.mjs";
-
-renderHeaderFooter();
+import NewsletterSignUp from "./components/NewsletterSignUp.svelte"
+import { updateCartIcon } from "./utils.mjs";
 
 /**
  * Create an instance of the ProductList svelte component, inserts the tent products into the home page for user to click on a product
@@ -11,6 +10,9 @@ new ProductList({
   target: document.querySelector(".products"),
   props: { category: "tents" },
 });
+new NewsletterSignUp({
+  target: document.getElementById("newsletter-sign-up")
+})
 
 // for the cart to update properly we need to check the contence of the cart
 // on the home page. If we dont, the display tag in CSS will stay on none
