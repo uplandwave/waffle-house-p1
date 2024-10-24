292f3e0430f0a6c1a8f04cce2571dc9504d8c943
starb520 <97459015+starb520@users.noreply.github.com>
2024-10-23 19:16:52 -0600

    second alert pop up edited so it is now a banner that displays at the top of the page, the user can close it if wanted

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,44 @@
 // main.js
 import ProductList from "./components/ProductList.svelte";
-
 import NewsletterSignUp from "./components/NewsletterSignUp.svelte";
 import { updateCartIcon, renderHeaderFooter } from "./utils.mjs";
+import { isFirstVisit, setFirstVisit } from "./alerts.js";
 
 renderHeaderFooter();
+
+function showBanner() {
+  const banner = document.getElementById("banner");
+  if (banner) {
+    {
+      banner.style.display = "block";
+    }
+  }
+}
+
+function hideBanner() {
+  const banner = document.getElementById("banner");
+  if (banner) {
+    banner.style.display = "none";
+  }
+}
+
+// for the cart to update properly we need to check the contence of the cart
+// on the home page. If we dont, the display tag in CSS will stay on none
+document.addEventListener("DOMContentLoaded", () => {
+  // Check if the user is visiting for the first time and show the banner
+  if (isFirstVisit()) {
+    showBanner();
+    setFirstVisit();
+  }
+
+  // Add event listener to close the banner when "X" is clicked
+  const closeBannerButton = document.getElementById("close-banner");
+  if (closeBannerButton) {
+    closeBannerButton.addEventListener("click", hideBanner);
+  }
+
+  updateCartIcon();
+});
 
 /**
  * Create an instance of the ProductList svelte component, inserts the tent products into the home page for user to click on a product
@@ -13,12 +47,7 @@ new ProductList({
   target: document.querySelector(".products"),
   props: { category: "tents" },
 });
+
 new NewsletterSignUp({
   target: document.getElementById("newsletter-sign-up"),
-});
-
-// for the cart to update properly we need to check the contence of the cart
-// on the home page. If we dont, the display tag in CSS will stay on none
-document.addEventListener("DOMContentLoaded", () => {
-  updateCartIcon();
 });
